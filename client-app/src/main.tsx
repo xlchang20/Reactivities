1b22308637c0bf43ffd5b320d54accfdfb4c6fde
xlchang <xlchang20@gmail.com>
2024-08-27 03:35:47 -0500

    End of Section 5

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -2,11 +2,12 @@
 // import { createRoot } from 'react-dom/client'
 // import App from './App.tsx'
 // import './index.css'
-import React from 'react';
+//import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './app/layout/App';
+import './app/layout/styles.css';
+//import reportWebVitals from './reportWebVitals';
+
 
 // createRoot(document.getElementById('root')!).render(
 //   <StrictMode>
