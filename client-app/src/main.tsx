733d5d9f831661714b7f8514b77d65d810c50c04
xlchang <xlchang20@gmail.com>
2024-08-29 02:11:46 -0500

    End of Section 7

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -2,20 +2,26 @@
 // import { createRoot } from 'react-dom/client'
 // import App from './App.tsx'
 // import './index.css'
-//import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './app/layout/App';
-import './app/layout/styles.css';
+import ReactDOM from 'react-dom'
+import App from './app/layout/App'
+import './app/layout/styles.css'
+import { store, StoreContext } from './app/stores/store';
 //import reportWebVitals from './reportWebVitals';
 
 
-// createRoot(document.getElementById('root')!).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
+//ReactDOM.render(
+  //<React.StrictMode>
+      //<StoreContext.Provider value={store}>
+        //<App />
+      //</StoreContext.Provider>,
+  //</React.StrictMode>,
+  //document.getElementById('root')!
 
 ReactDOM.render(
-  <App />,
-document.getElementById('root')
+  //<React.StrictMode>
+    <StoreContext.Provider value={store}>
+      <App />
+    </StoreContext.Provider>,
+  //</React.StrictMode>,
+  document.getElementById('root')
 );
